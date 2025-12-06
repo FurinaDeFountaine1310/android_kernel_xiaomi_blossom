aaf5148648d9c6c139b5fbe76fb514adcfeb8b1c
Carlotta-Montelli <carlotta.montelli.0201@gmail.com>
2025-12-07 00:15:14 +0700

    kernel: module: fix build

--- kernel/module.c
+++ kernel/module.c
@@ -622,7 +622,8 @@ static struct module *find_module_all(const char *name, size_t len,
 
 struct module *find_module(const char *name)
 {
-	module_assert_mutex();
+	mutex_lock(&module_mutex);
+	mutex_unlock(&module_mutex);
 	return find_module_all(name, strlen(name), false);
 }
 EXPORT_SYMBOL_GPL(find_module);
